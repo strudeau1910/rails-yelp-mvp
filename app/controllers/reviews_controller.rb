f98fa963c6ff84aae77143eff6c6a101ddebab0a
Stephanie Trudeau <strudeau1910@gmail.com>
2018-08-08 18:11:55 -0400

    reviews completed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,23 @@
 class ReviewsController < ApplicationController
+
   def new
     # we need @restaurant in our `simple_form_for`
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+
     @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+
     # we need `restaurant_id` to asssociate review with corresponding restaurant
-    @review.restaurant = Restaurant.find(params[:restaurant_id])
-    @review.save
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   private
@@ -18,3 +26,6 @@ class ReviewsController < ApplicationController
     params.require(:review).permit(:content, :rating )
   end
 end
+
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#puts 'Cleaning database...'
 Restaurant.destroy_all
-#puts 'Creating restaurants...'
 restaurants_attributes = [
   {
     name:         'Dishoom',
